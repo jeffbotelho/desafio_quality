cef6d45e7147a063675532400fb2e08acc27b9ef
Jefferson <jefferson.botelho@mercadolivre.com>
2022-03-29 15:15:29 -0300

    initial commit

---
+++ src/main/java/br/com/meli/seu_imovel/SeuImovelApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.seu_imovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuImovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeuImovelApplication.class, args);
+    }
+
+}
