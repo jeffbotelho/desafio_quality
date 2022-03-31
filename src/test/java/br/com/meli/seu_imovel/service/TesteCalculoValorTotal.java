6695c655e8cf641402cfbadae27d86a79cbad47a
Weverton Bruno <weverton.ssantos@mercadolivre.com>
2022-03-31 08:41:01 -0300

    chore: Comentando metodos e fases do teste

--- src/test/java/br/com/meli/seu_imovel/service/TesteCalculoValorTotal.java
+++ src/test/java/br/com/meli/seu_imovel/service/TesteCalculoValorTotal.java
@@ -31,17 +31,25 @@ public class TesteCalculoValorTotal {
     @Test
     @DisplayName("Teste se cálculo do valor total da propriedade é realizado corretamente:")
     public void testeCalculoValorTotal() {
+        // Setup do teste: gera um objeto de request e mocka repository
         ImovelDTO imovelDTO = geraImovelTeste();
         Mockito.when(bairroRepository.findByName("centro")).thenReturn(Optional.of("centro"));
+
+        // Act: Execucao do service
         ImovelDTO returnedImovelDTO = service.gerarRelatorio(imovelDTO);
+
+        // Assert: verifica se o valor do imovel esta conforme o esperado
         Assertions.assertEquals(returnedImovelDTO.getTotalPrice(), BigDecimal.valueOf(5300000D));
     }
 
     @Test
     @DisplayName("Teste se uma exceção é lançada quando o bairro não consta no repositório")
     public void testeSeLancaExcecaoBairroNaoEncontrado() {
+        // Setup do teste: gera um objeto de request e mocka repository
         ImovelDTO imovelDTO = geraImovelTeste();
         Mockito.when(bairroRepository.findByName("centro")).thenReturn(Optional.empty());
+
+        // Act & Assertion: Verifica se ao executar o service uma excecao eh lancada por nao encontrar o bairro
         Throwable exceptionReturned = Assertions.assertThrows(DistrictNotExistExepection.class, () -> service.gerarRelatorio(imovelDTO));
         Assertions.assertEquals(exceptionReturned.getMessage(), "Distrito não válido!");
     }
@@ -49,14 +57,23 @@ public class TesteCalculoValorTotal {
     @Test
     @DisplayName("Testar se o valor do nome do bairro consultado no repositório é o mesmo passado na requisição")
     public void testeSeValorConsultaBairroIgualAoEnviadoNaRequisicao() {
+        // Setup do teste: Gera um objeto de request e mocka repositorio
         ImovelDTO imovelDTO = geraImovelTeste();
         Mockito.when(bairroRepository.findByName(Mockito.anyString())).thenReturn(Optional.of("centro"));
+
+        // Act: Execucao do service
         service.gerarRelatorio(imovelDTO);
+
+        // Assert: VErifica se o metodo findByName do repository é chamado 1 vez e
+        // verifica se esse metodo é chamado passando como argumento o valor "centro"
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
         Mockito.verify(bairroRepository, Mockito.times(1)).findByName(captor.capture());
         Assertions.assertEquals("centro", captor.getValue());
     }
 
+    /*
+    * Metodo auxiliar para gerar um objeto de request com algumas informacoes predefinidas para teste
+    * */
     private ImovelDTO geraImovelTeste() {
         ImovelDTO imovelDTO = new ImovelDTO();
         imovelDTO.setPropName("Imovel1");
