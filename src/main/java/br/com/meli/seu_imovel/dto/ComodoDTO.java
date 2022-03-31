19621928bbf7baa84a37855e133e73b586a9ac7e
Weverton Bruno <weverton.ssantos@mercadolivre.com>
2022-03-31 14:39:46 -0300

    feat: Teste de validacoes da largura do quarto

--- src/main/java/br/com/meli/seu_imovel/dto/ComodoDTO.java
+++ src/main/java/br/com/meli/seu_imovel/dto/ComodoDTO.java
@@ -14,17 +14,17 @@ import javax.validation.constraints.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ComodoDTO {
-    @NotBlank(message = "O campo não pode estar vazio. (nome do quarto)")
+    @NotBlank(message = "O campo não pode estar vazio. (Nome do quarto)")
     @Pattern(regexp=
             "[A-ZÀ-Ú][A-z À-ú0-9]*",
-            message = "O campo deve começar com uma letra maiúscula."
+            message = "O campo deve começar com uma letra maiúscula. (Nome do quarto)"
     )
-    @Size(max = 30,message = "O campo não pode exceder 30 caracteres.")
+    @Size(max = 30,message = "O campo não pode exceder 30 caracteres. (Nome do quarto)")
     private String roomName;
-    @NotNull(message = "A largura não pode estar vazia. ()")
+    @NotNull(message = "A largura não pode estar vazia. (Largura do Quarto)")
     @Max(value = 25,message = "A largura máxima permitida por cômodo é de 25 metros.")
     private Double roomWidth;
-    @NotNull(message = "O comprimento não pode estar vázio.")
+    @NotNull(message = "O comprimento não pode estar vázio. (Comprimento do quarto)")
     @Max(value = 33,message = "O comprimento máximo permitidio por cômodo é de 33 metros.")
     private Double roomLength;
     private Double area;
